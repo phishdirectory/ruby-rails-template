ac7cfdfb4ff5906764129861629ec5abefb27026
Jasper Mayone <jasper.mayone@phish.directory>
2025-05-04 20:46:54 -0400

    Update production.rb
    Signed-off-by: Jasper Mayone <jasper.mayone@phish.directory>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
+  # Prepend all log lines with the following tags.
+  config.log_tags = [:request_id]
+
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
 
@@ -49,7 +52,8 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
+  # config.cache_store = :solid_cache_store
+  config.cache_store = :redis_cache_store, { url: ENV["REDIS_CACHE_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
