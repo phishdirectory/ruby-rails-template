0c14fcaab6ff6246f036d29ce0ea6cace8c683ed
Jasper Mayone <jasper.mayone@phish.directory>
2025-05-05 15:52:08 -0400

    Create blind_index.rb
    Signed-off-by: Jasper Mayone <jasper.mayone@phish.directory>

---
+++ config/initializers/blind_index.rb
@@ -0,0 +1,7 @@
+# config/initializers/blind_index.rb
+require 'blind_index'
+
+# Generate a secure key for blind index (32 bytes / 64 hex chars)
+# In production, this should come from environment variables or credentials
+BlindIndex.master_key = Rails.application.credentials.dig(:blind_index, :master_key) || 
+                       SecureRandom.hex(32)
